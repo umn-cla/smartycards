5d3edee60b7efd2a60575827e8e021c3bfefbe0d
James Johnson <johnsojr@umn.edu>
2024-08-09 20:38:51 -0500

    fix redirect to build as base

--- resources/client/router/index.ts
+++ resources/client/router/index.ts
@@ -1,140 +1,140 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/HomePage/HomePage.vue';
-import { useQueryClient } from '@tanstack/vue-query';
-import * as api from '@/api';
-import { PROFILE_QUERY_KEY } from '@/queries/queryKeys';
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage/HomePage.vue";
+import { useQueryClient } from "@tanstack/vue-query";
+import * as api from "@/api";
+import { PROFILE_QUERY_KEY } from "@/queries/queryKeys";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomePage,
     },
     {
-      path: '/auth/login',
-      name: 'auth.login',
-      component: () => import('../pages/Auth/LoginPage.vue'),
+      path: "/auth/login",
+      name: "auth.login",
+      component: () => import("../pages/Auth/LoginPage.vue"),
     },
     {
-      path: '/auth/logout',
-      name: 'auth.logout',
-      component: () => import('../pages/Auth/LogoutPage.vue'),
+      path: "/auth/logout",
+      name: "auth.logout",
+      component: () => import("../pages/Auth/LogoutPage.vue"),
     },
     {
-      path: '/auth/callback',
-      name: 'auth.callback',
-      redirect: '/decks',
+      path: "/auth/callback",
+      name: "auth.callback",
+      redirect: "/decks",
     },
     {
-      path: '/decks',
-      name: 'decks.index',
-      component: () => import('../pages/Decks/DeckIndexPage'),
+      path: "/decks",
+      name: "decks.index",
+      component: () => import("../pages/Decks/DeckIndexPage"),
       meta: {
         requireAuth: true,
       },
     },
     {
-      path: '/decks/:deckId',
-      name: 'decks.show',
-      component: () => import('../pages/Decks/DeckShowPage.vue'),
+      path: "/decks/:deckId",
+      name: "decks.show",
+      component: () => import("../pages/Decks/DeckShowPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
       }),
     },
     {
-      path: '/decks/create',
-      name: 'decks.create',
-      component: () => import('../pages/Decks/CreateDeckPage.vue'),
+      path: "/decks/create",
+      name: "decks.create",
+      component: () => import("../pages/Decks/CreateDeckPage.vue"),
     },
     {
-      path: '/decks/:deckId/edit',
-      name: 'decks.edit',
-      component: () => import('../pages/Decks/EditDeckPage.vue'),
+      path: "/decks/:deckId/edit",
+      name: "decks.edit",
+      component: () => import("../pages/Decks/EditDeckPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
       }),
     },
     {
-      path: '/decks/:deckId/import',
-      name: 'decks.import',
-      component: () => import('../pages/Decks/ImportDeckCardsPage.vue'),
+      path: "/decks/:deckId/import",
+      name: "decks.import",
+      component: () => import("../pages/Decks/ImportDeckCardsPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
       }),
     },
     {
-      path: '/decks/:deckId/share',
-      name: 'decks.share',
-      component: () => import('../pages/Decks/ShareDeckPage.vue'),
+      path: "/decks/:deckId/share",
+      name: "decks.share",
+      component: () => import("../pages/Decks/ShareDeckPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
       }),
     },
     {
-      path: '/decks/:deckId/invite',
-      name: 'decks.invite',
-      component: () => import('../pages/Decks/InviteToDeckPage.vue'),
+      path: "/decks/:deckId/invite",
+      name: "decks.invite",
+      component: () => import("../pages/Decks/InviteToDeckPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
         url: route.query.url,
       }),
     },
     {
-      path: '/profile',
-      name: 'profile',
-      component: () => import('../pages/ProfilePage.vue'),
+      path: "/profile",
+      name: "profile",
+      component: () => import("../pages/ProfilePage.vue"),
     },
     {
-      path: '/decks/:deckId/cards/create',
-      name: 'cards.create',
-      component: () => import('../pages/Cards/CreateOrEditCardPage.vue'),
+      path: "/decks/:deckId/cards/create",
+      name: "cards.create",
+      component: () => import("../pages/Cards/CreateOrEditCardPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
       }),
     },
     {
-      path: '/decks/:deckId/cards/:cardId/edit',
-      name: 'cards.edit',
-      component: () => import('../pages/Cards/CreateOrEditCardPage.vue'),
+      path: "/decks/:deckId/cards/:cardId/edit",
+      name: "cards.edit",
+      component: () => import("../pages/Cards/CreateOrEditCardPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
         cardId: Number(route.params.cardId),
       }),
     },
     {
-      path: '/decks/:deckId/practice',
-      name: 'decks.practice',
-      component: () => import('../pages/Decks/PracticeDeckPage.vue'),
+      path: "/decks/:deckId/practice",
+      name: "decks.practice",
+      component: () => import("../pages/Decks/PracticeDeckPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
       }),
     },
     {
-      path: '/decks/:deckId/practice/summary',
-      name: 'decks.practice.summary',
-      component: () => import('@/pages/Decks/PracticeSummaryPage.vue'),
+      path: "/decks/:deckId/practice/summary",
+      name: "decks.practice.summary",
+      component: () => import("@/pages/Decks/PracticeSummaryPage.vue"),
       props: (route) => ({
         deckId: Number(route.params.deckId),
       }),
     },
     {
-      path: '/admin',
-      name: 'admin',
-      component: () => import('@/pages/AdminPage.vue'),
+      path: "/admin",
+      name: "admin",
+      component: () => import("@/pages/AdminPage.vue"),
     },
     {
-      path: '/test/editor',
-      name: 'test.editor',
-      component: () => import('@/pages/Test/EditorPage.vue'),
+      path: "/test/editor",
+      name: "test.editor",
+      component: () => import("@/pages/Test/EditorPage.vue"),
       props: true,
     },
     // catch all 404
     {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: () => import('../pages/NotFoundPage.vue'),
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: () => import("../pages/NotFoundPage.vue"),
     },
   ],
 });
@@ -157,7 +157,7 @@ router.beforeEach(async (to, from, next) => {
         const user = await api.getCurrentUser({ skipErrorNotifications: true });
         return user;
       } catch (error) {
-        console.error('Route guard: error getting current user', error);
+        console.error("Route guard: error getting current user", error);
         return null;
       }
     },
@@ -168,7 +168,7 @@ router.beforeEach(async (to, from, next) => {
   if (!isAuthenticated) {
     // Redirect to login if not authenticated
     return next({
-      name: 'auth.login',
+      name: "auth.login",
       query: { redirect: to.fullPath },
     });
   }
