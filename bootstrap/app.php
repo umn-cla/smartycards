35f3e3ffcd816aa4d0bae39f5103509bfcb6b68c
Colin McFadden <colin@hedge.video>
2024-08-14 14:48:58 -0500

    exempt local-sp for csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            '/local-sp/ACS'
+        ]);
 
         //
     })
