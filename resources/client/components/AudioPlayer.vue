9141ea2ea9a6401baba52e6d7763f1c7a35ffd79
James Johnson <johnsojr@umn.edu>
2024-10-16 16:22:31 -0500

    pause audio player if scrolled out of view

--- resources/client/components/AudioPlayer.vue
+++ resources/client/components/AudioPlayer.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="">
-    <audio :src="src" controls class="max-w-full"></audio>
+    <audio :src="src" controls class="max-w-full" ref="audioRef"></audio>
   </div>
 </template>
 <script setup lang="ts">
+import { useIntersectionObserver } from "@vueuse/core";
+import { ref } from "vue";
+
 defineProps<{
   src: string;
 }>();
+
+const audioRef = ref<HTMLAudioElement | null>(null);
+
+useIntersectionObserver(
+  audioRef,
+  ([{ isIntersecting }]) => {
+    if (isIntersecting) {
+      return;
+    }
+
+    // pause if scrolled out of view
+    audioRef.value?.pause();
+  },
+  {
+    threshold: 0.5,
+  },
+);
 </script>
 <style scoped></style>
