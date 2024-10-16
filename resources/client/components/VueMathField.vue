e26115d9ec1d9d27fb34e26281ba82e352c67556
James Johnson <johnsojr@umn.edu>
2024-10-16 10:31:09 -0500

    add math input block

--- resources/client/components/VueMathField.vue
+++ resources/client/components/VueMathField.vue
@@ -1,59 +1,39 @@
 <template>
-  <math-field ref="mathFieldRef" class="w-full" @input="handleInput">
-    {{ localValue }}
+  <math-field
+    ref="mathFieldRef"
+    class="math-field w-full bg-black/5"
+    @input="handleInput"
+  >
+    {{ modelValue }}
   </math-field>
 </template>
 
 <script setup lang="ts">
-import { nextTick, onMounted, ref } from "vue";
+import { ref } from "vue";
 import { MathfieldElement } from "mathlive";
 
-const props = defineProps<{
+defineProps<{
   modelValue: string;
 }>();
 
-const localValue = ref(props.modelValue);
+const emit = defineEmits<{
+  (event: "update:modelValue", value: string): void;
+}>();
 
 const mathFieldRef = ref<MathfieldElement | null>(null);
 
 function handleInput(event: Event) {
   console.log("handleInput", {
-    value: mathFieldRef.value?.getAttribute("value"),
+    value: mathFieldRef.value?.value,
     event: event,
     el: mathFieldRef.value,
   });
+  emit("update:modelValue", mathFieldRef.value?.value || "");
 }
-
-onMounted(() => {
-  if (!mathFieldRef.value) {
-    throw new Error("Mathfield element not found");
-  }
-  mathFieldRef.value.addEventListener("input", (e) => {
-    console.log("addedEventListerer trigger");
-    handleInput(e);
-  });
-});
-
-// function handleKeypress(event: Event) {
-//   if (!mathFieldRef.value) return;
-//   console.log({
-//     value: mathFieldRef.value.value,
-//     event: event,
-//     el: mathFieldRef.value,
-//   });
-//   nextTick(() => {
-//     if (!mathFieldRef.value) return;
-//     localValue.value = mathFieldRef.value.value;
-//     console.log(localValue.value);
-//   });
-// }
-
-// onMounted(() => {
-//   if (!mathFieldRef.value) {
-//     throw new Error("Mathfield element not found");
-//   }
-
-//   // mathFieldRef.value.addEventListener("input", handleInput);
-//   mathFieldRef.value.addEventListener("keypress", handleKeypress);
-// });
 </script>
+<style>
+math-field::part(virtual-keyboard-toggle),
+math-field::part(menu-toggle) {
+  @apply text-neutral-500 hover:text-neutral-900;
+}
+</style>
