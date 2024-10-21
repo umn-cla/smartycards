54b92239138950770a999e1f212cae4d77826913
James Johnson <johnsojr@umn.edu>
2024-10-21 10:55:26 -0500

    fix scrollbars on safari

--- resources/client/pages/Games/MatchingGame/MatchingSide.vue
+++ resources/client/pages/Games/MatchingGame/MatchingSide.vue
@@ -1,47 +1,35 @@
 <template>
   <div
-    class="bg-brand-oatmeal-50 shadow-sm border border-brand-oatmeal-300 rounded-sm p-1 aspect-square overflow-y-auto scrollbar-thin scrollbar-thumb-black/10 scrollbar-track-transparent items-center relative"
+    class="bg-brand-oatmeal-50 shadow-sm border border-brand-oatmeal-300 rounded-sm aspect-square overflow-y-auto scrollbar-thin scrollbar-thumb-black/10 scrollbar-track-transparent relative flex flex-col h-full w-full items-center justify-center p-1"
   >
-    <!-- <Button
-      variant="secondary"
-      class="absolute top-1 right-1 z-20 bg-neutral-50"
-      >Zoom</Button
-    > -->
-    <div class="flex flex-col h-full w-full items-center justify-center">
-      <template v-for="block in blocks" :key="block.id">
-        <TextBlockView
-          v-if="isTextBlock(block)"
-          :block="block"
-          class="text-xs sm:text-base !leading-[1] pointer-events-none"
-        />
-        <ImageBlockView
-          v-else-if="isImageBlock(block)"
-          :src="block.content"
-          :alt="block.meta?.alt ?? ''"
-          :withLightbox="false"
-        />
-        <VideoBlockView v-else-if="isVideoBlock(block)" :block="block" />
-        <EmbedBlockView v-else-if="isEmbedBlock(block)" :block="block" />
-        <!-- <AudioBlockView v-else-if="isAudioBlock(block)" :block="block" /> -->
-        <SimpleAudioPlayer
-          v-else-if="isAudioBlock(block)"
-          :src="block.content"
-        />
-        <HintBlockView
-          v-else-if="isHintBlock(block)"
-          :modelValue="block.content"
-          :meta="block.meta"
-        />
-        <MathBlockView v-else-if="isMathBlock(block)" :block="block" />
-        <UnknownBlockView v-else :block="block" />
-      </template>
-    </div>
+    <template v-for="block in blocks" :key="block.id">
+      <TextBlockView
+        v-if="isTextBlock(block)"
+        :block="block"
+        class="text-xs sm:text-base !leading-[1] pointer-events-none"
+      />
+      <ImageBlockView
+        v-else-if="isImageBlock(block)"
+        :src="block.content"
+        :alt="block.meta?.alt ?? ''"
+        :withLightbox="false"
+      />
+      <VideoBlockView v-else-if="isVideoBlock(block)" :block="block" />
+      <EmbedBlockView v-else-if="isEmbedBlock(block)" :block="block" />
+      <!-- <AudioBlockView v-else-if="isAudioBlock(block)" :block="block" /> -->
+      <SimpleAudioPlayer v-else-if="isAudioBlock(block)" :src="block.content" />
+      <HintBlockView
+        v-else-if="isHintBlock(block)"
+        :modelValue="block.content"
+        :meta="block.meta"
+      />
+      <MathBlockView v-else-if="isMathBlock(block)" :block="block" />
+      <UnknownBlockView v-else :block="block" />
+    </template>
   </div>
 </template>
 <script setup lang="ts">
 import * as T from "@/types";
-import { computed } from "vue";
-import { Button } from "@/components/ui/button";
 import {
   isImageBlock,
   isTextBlock,
