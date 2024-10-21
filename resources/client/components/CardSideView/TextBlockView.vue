c7fb1641aac93b23662eefaaaaf5109f83368d13
James Johnson <johnsojr@umn.edu>
2024-10-21 16:24:05 -0500

    fix(block-editor): lists and links don't have formatting

--- resources/client/components/CardSideView/TextBlockView.vue
+++ resources/client/components/CardSideView/TextBlockView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex items-center justify-center">
+  <div class="text-block-view flex items-center justify-center">
     <div
       v-html="block.content"
       @click.stop
       ref="blockElRef"
       :class="
         cn(
-          'text-block-view font-serif leading-5',
+          'font-serif leading-5',
           {
             'text-center': wordCount < 5,
           },
@@ -28,4 +28,25 @@ const props = defineProps<{
 
 const wordCount = computed(() => props.block.content.split(/\s+/).length);
 </script>
-<style></style>
+<style type="post-css">
+/**
+ * Style fixes for lists:
+ * 1. quill makes all lists `ol and adds a `data-list` attr depending
+ * 2. also, tailwind strips styles
+ */
+.text-block-view :is(ol, ul) {
+  padding-left: 1.5em;
+}
+
+.text-block-view [data-list="bullet"] {
+  list-style-type: disc;
+}
+
+.text-block-view [data-list="ordered"] {
+  list-style-type: decimal;
+}
+
+.text-block-view a {
+  @apply text-brand-teal-500 underline;
+}
+</style>
