02e6a4fc5408a00904eefe6f405022f7da844ec1
James Johnson <johnsojr@umn.edu>
2024-10-30 15:22:32 -0500

    add favicon and mobile touch icons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,11 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="theme-color" content="#2E091E">
+  <link rel="icon" href="favicon.svg">
+  <link rel="mask-icon" href="mask-icon.svg" color="#2E091E">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="manifest" href="manifest.json">
 
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
