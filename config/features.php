26edd401a51f186cebc515e52d0e1ee4665ce853
James Johnson <johnsojr@umn.edu>
2024-11-22 13:00:29 -0600

    make key name consistent

--- config/features.php
+++ config/features.php
@@ -3,5 +3,5 @@
 // This file is used to store the feature flags for the application.
 
 return [
-    'text_to_speech' => env('ENABLE_FEATURE_TEXT_TO_SPEECH', false),
+    'text_to_speech' => env('AZURE_TTS_ENABLE', false),
 ];
