f6ee205fe2a1a80afdd10861af97741e53e9558a
James Johnson <johnsojr@umn.edu>
2025-01-10 17:52:56 -0600

    revert

--- resources/client/components/CardAttemptChoices.vue
+++ resources/client/components/CardAttemptChoices.vue
@@ -5,21 +5,18 @@
     <button
       @click="handleAnswer(1)"
       class="flex items-center justify-center bg-brand-maroon-800/5 py-4 px-8 rounded-lg leading-none hover:bg-brand-maroon-800/10"
-      data-cy="button-answer-score-1"
     >
       ❌
     </button>
     <button
       @click="handleAnswer(2)"
       class="flex items-center justify-center bg-brand-maroon-800/5 py-4 px-8 rounded-lg leading-none hover:bg-brand-maroon-800/10"
-      data-cy="button-answer-score-2"
     >
       🫤
     </button>
     <button
       class="flex items-center justify-center bg-brand-maroon-800/5 py-4 px-8 rounded-lg leading-none hover:bg-brand-maroon-800/10"
       @click="handleAnswer(3)"
-      data-cy="button-answer-score-3"
     >
       ✅
     </button>
