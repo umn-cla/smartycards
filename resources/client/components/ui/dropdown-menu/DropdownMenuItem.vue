f3f4149442fd6be0ee03e98833d552f7bdc11a2c
James Johnson <johnsojr@umn.edu>
2025-01-21 18:47:03 -0600

    fix focus state

--- resources/client/components/ui/dropdown-menu/DropdownMenuItem.vue
+++ resources/client/components/ui/dropdown-menu/DropdownMenuItem.vue
@@ -25,7 +25,7 @@ const forwardedProps = useForwardProps(delegatedProps);
     v-bind="forwardedProps"
     :class="
       cn(
-        'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-stone-100 focus:text-brand-maroon-800 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-stone-800 dark:focus:text-stone-50',
+        'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-stone-100 focus:ring-2 focus-visible:ring-2 focus-visible:ring-blue-600 focus:ring-blue-600 active:ring-2 active:ring-blue-600 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-stone-800 dark:focus:text-stone-50',
         inset && 'pl-8',
         props.class,
       )
