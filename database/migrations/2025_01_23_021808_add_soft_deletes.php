6d3ff0e1976c81432c83b079cb70cf8b18c41a98
James Johnson <johnsojr@umn.edu>
2025-01-23 13:10:05 -0600

    remove soft deletes for deck_memberships in migration

--- database/migrations/2025_01_23_021808_add_soft_deletes.php
+++ database/migrations/2025_01_23_021808_add_soft_deletes.php
@@ -22,10 +22,6 @@ return new class extends Migration
         Schema::table('cards', function (Blueprint $table) {
             $table->softDeletes();
         });
-
-        Schema::table('deck_memberships', function (Blueprint $table) {
-            $table->softDeletes();
-        });
     }
 
     /**
@@ -42,10 +38,6 @@ return new class extends Migration
         });
 
         Schema::table('cards', function (Blueprint $table) {
-            $table->dropSoftDeletes();
-        });
-
-        Schema::table('deck_memberships', function (Blueprint $table) {
             $table->dropSoftDeletes();
         });
     }
