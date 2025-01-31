b2a530c4636911c7add95206a19f102bc17108d8
James Johnson <johnsojr@umn.edu>
2025-01-31 12:11:36 -0600

    render markdown with LaTeX
    think this might be better than MathML, but we'll see

---
+++ resources/client/components/Markdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-html="html" />
+</template>
+<script setup lang="ts">
+import { computed } from "vue";
+import { marked } from "marked";
+import markedKatex from "marked-katex-extension";
+
+const props = defineProps<{
+  content: string;
+}>();
+
+const options = {
+  throwOnError: false,
+};
+
+marked.use(markedKatex(options));
+
+const html = computed(() => {
+  const html = marked.parse(props.content);
+  return html;
+});
+</script>
+<style scoped></style>
