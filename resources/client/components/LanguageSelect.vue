6867d83b6592f1be4dd97045b0cb7528e48f88f0
James Johnson <johnsojr@umn.edu>
2025-03-12 14:37:56 -0500

    extract language select component

---
+++ resources/client/components/LanguageSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <Select
+    :id="id"
+    :modelValue="modelValue ?? ''"
+    @update:modelValue="
+      (locale: LanguageOption['locale'] | '') =>
+        $emit('update:modelValue', locale || null)
+    "
+  >
+    <SelectTrigger class="bg-brand-maroon-800/5">
+      <SelectValue placeholder="Language (Auto)"> </SelectValue>
+    </SelectTrigger>
+    <SelectContent>
+      <SelectItem
+        v-for="lang in ttsLanguageOptions"
+        :value="lang.locale"
+        :key="lang.locale"
+      >
+        {{ lang.name }}
+      </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
+<script setup lang="ts">
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
+import { ttsLanguageOptions } from "@/lib/ttsLanguageOptions";
+import { LanguageOption } from "@/types";
+
+defineProps<{
+  id: string;
+  modelValue: LanguageOption["locale"] | null;
+}>();
+
+defineEmits<{
+  (event: "update:modelValue", value: LanguageOption["locale"] | null): void;
+}>();
+</script>
+<style scoped></style>
