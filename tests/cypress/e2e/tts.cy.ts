2a2f4ba32ef428e6421fa05fac2a506ff8a9c417
James Johnson <johnsojr@umn.edu>
2025-03-13 11:25:18 -0500

    WIP: start adding tests to prevent breakage

---
+++ tests/cypress/e2e/tts.cy.ts
@@ -0,0 +1,17 @@
+describe("Text Block", () => {
+  it("shows the TTS player if TTS is enabled");
+
+  it("does not show the TTS player if TTS is disabled");
+
+  it(
+    "defaults to an `auto` language if no language is set for the block or the deck side",
+  );
+
+  it("permits setting a language for the block");
+
+  it("plays the block content in the selected language");
+
+  it("uses the deck side default language if no language is set");
+
+  it("overrides the deck side default language if a language is set");
+});
