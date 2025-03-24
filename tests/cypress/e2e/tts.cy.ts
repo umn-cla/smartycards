e7f59ba7bc166fea05bbab78a5928aa794151cfb
James Johnson <johnsojr@umn.edu>
2025-03-24 17:49:32 -0500

    test that deck side defaults are used if no card language is set

--- tests/cypress/e2e/tts.cy.ts
+++ tests/cypress/e2e/tts.cy.ts
@@ -96,18 +96,99 @@ describe("Text Block", () => {
         });
     });
 
-    it("permits setting a language for the block", () => {
-      cy.visit(`/decks/${deckId}`);
+    it("uses deck side locale defaults if no card language is set", () => {
+      // set the deck side default language
+      cy.visit(`/decks/${deckId}/edit`);
+
+      // wait for deck info to load
+      cy.get("#name").should("have.value", "My Deck");
+
+      // set the deck side default language
+      cy.visit(`/decks/${deckId}/edit`);
+
+      // wait for deck info to load
+      cy.get("#name").should("have.value", "My Deck");
+
+      // Click to open the select dropdown
+      cy.get("#default-front-locale")
+        .select("fr-FR")
+        .should("have.value", "fr-FR");
+
+      cy.get("#default-back-locale")
+        .select("es-MX")
+        .should("contain.text", "Spanish (Mexico)");
+
+      // save the deck
+      cy.intercept("PUT", `/api/decks/${deckId}`).as("updateDeck");
+      cy.contains("button", "Save").click();
+
+      // wait for the request to complete
+      cy.wait("@updateDeck");
+
+      // we should now be on the deck index page
+      cy.location("pathname").should("eq", `/decks/${deckId}`);
+
+      // check that the TTS Player is visible on the first card
+      // and is using French as the language
+      cy.intercept("POST", "/api/tts").as("ttsRequest");
       cy.contains("Front side 0")
         .parent()
         .within(() => {
-          // cy.get('[data-cy="simple-tts-player"]').click();
-          // cy.get('[data-cy="tts-language-select"]').select("en");
-          // cy.get('[data-cy="simple-tts-player"] .sr-only').should(
-          //   "have.text",
-          //   "English",
-          // );
+          cy.get('[data-cy="simple-tts-player"]')
+            .should("be.visible")
+            .should("contain.text", "French")
+            .click();
+          cy.wait("@ttsRequest");
+          cy.get('[data-cy="simple-tts-player"]').should(
+            "have.attr",
+            "aria-pressed",
+            "true",
+          );
         });
+
+      // flip the card
+      cy.contains("Front side 0")
+        .closest('[data-cy="flippable-card"]')
+        .within(() => {
+          cy.contains("Flip").click();
+        });
+
+      // wait for the card to flip
+      cy.wait(3000);
+
+      // check the back side of the card is using Spanish
+      cy.contains("Back side 0")
+        .closest('[data-cy="card-side-view--Back"]')
+        // workaround an issue where cypress thinks the tts
+        // player is not visible when the card is flipped
+        .invoke("css", "backface-visibility", "visible")
+        .within(() => {
+          cy.get('[data-cy="simple-tts-player"]')
+            .should("be.visible")
+            .should("contain.text", "Spanish (Mexico)")
+            .click();
+
+          cy.wait("@ttsRequest");
+          cy.get('[data-cy="simple-tts-player"]').should(
+            "have.attr",
+            "aria-pressed",
+            "true",
+          );
+        });
+    });
+
+    it("permits setting a language for the block", () => {
+      cy.visit(`/decks/${deckId}`);
+      // cy.contains("Front side 0")
+      //   .parent()
+      //   .within(() => {
+      //     cy.get('[data-cy="simple-tts-player"]').click();
+      //     cy.get('[data-cy="tts-language-select"]').select("fr-FR");
+      //     cy.get('[data-cy="simple-tts-player"]').should(
+      //       "have.text",
+      //       "English",
+      //     );
+      //   });
     });
 
     it("plays the block content in the selected language");
