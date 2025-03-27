90ea19c27916c6e1376f74f88c8c8a195525e791
James Johnson <johnsojr@umn.edu>
2025-03-26 22:39:43 -0500

    add microphone and stop icons

--- resources/client/components/AudioRecorder/RecordButton.vue
+++ resources/client/components/AudioRecorder/RecordButton.vue
@@ -11,53 +11,19 @@
     :title="isRecording ? 'Stop recording' : 'Start recording'"
   >
     <!-- Microphone icon (when not recording) -->
-    <svg
-      v-if="!isRecording"
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-5 w-5"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
-      />
-    </svg>
-
-    <!-- Stop icon (when recording) -->
-    <svg
-      v-else
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-5 w-5"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z"
-      />
-    </svg>
+    <IconMicrophone v-if="!isRecording" class="size-6" />
+    <IconStop v-else class="size-6" />
   </button>
 </template>
 
 <script setup lang="ts">
+import { IconMicrophone, IconStop } from "@/components/icons";
+
 defineProps<{
   isRecording: boolean;
 }>();
 
 defineEmits<{
-  (e: "click"): void;
+  (event: "click"): void;
 }>();
 </script>
