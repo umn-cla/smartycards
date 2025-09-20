f21cf7a1ac887a9c088b951ae971de19d60c0553
James Johnson <johnsojr@umn.edu>
2025-09-20 15:56:04 -0500

    Handle missing headers in card import Fixes #146

--- app/Imports/CardsImport.php
+++ app/Imports/CardsImport.php
@@ -5,12 +5,17 @@ namespace App\Imports;
 use App\Models\Card;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Str;
 
-class CardsImport implements SkipsEmptyRows, ToModel, WithHeadingRow
+class CardsImport implements SkipsEmptyRows, ToModel
 {
     protected $deckId;
+    protected $currentRow = 0;
+    protected $frontColumnIndex = 0;
+    protected $backColumnIndex = 1;
+
+    const FRONT_HEADER = 'front';
+    const BACK_HEADER = 'back';
 
     public function __construct($deckId)
     {
@@ -27,25 +32,55 @@ class CardsImport implements SkipsEmptyRows, ToModel, WithHeadingRow
         ];
     }
 
+    protected function isFirstRow(): bool
+    {
+        return $this->currentRow === 1;
+    }
+
+    protected function rowContainsHeaders($row): bool
+    {
+        return $row[0] === self::FRONT_HEADER && $row[1] === self::BACK_HEADER || $row[0] === self::BACK_HEADER && $row[1] === self::FRONT_HEADER;
+    }
+
+    protected function setupFrontBackColumnIndices($row): void
+    {
+        // swap column indices if headers are [back, front]
+        if ($row[0] === self::BACK_HEADER && $row[1] === self::FRONT_HEADER) {
+            $this->frontColumnIndex = 1;
+            $this->backColumnIndex = 0;
+        }
+    }
+
     /**
      * @return \Illuminate\Database\Eloquent\Model|null
      */
     public function model(array $row)
     {
+        $this->currentRow++;
+
+        if ($this->isFirstRow() && $this->rowContainsHeaders($row)) {
+            // swap front and back indices if needed
+            $this->setupFrontBackColumnIndices($row);
+            // don't import the header row as card
+            return;
+        }
+
+        $front = trim($row[$this->frontColumnIndex]);
+        $back = trim($row[$this->backColumnIndex]);
 
         return new Card([
             'deck_id' => $this->deckId,
             //create a single block for each side of the card
-            'front' => [$this->createBlock($row['front'])],
-            'back' => [$this->createBlock($row['back'])],
+            'front' => [$this->createBlock($front)],
+            'back' => [$this->createBlock($back)],
         ]);
     }
 
     public function rules(): array
     {
         return [
-            'front' => 'required|string',
-            'back' => 'required|string',
+            '0' => 'required|string',
+            '1' => 'required|string',
         ];
     }
 }
