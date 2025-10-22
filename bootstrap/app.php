3112da5dd49b82ded4f766e74afa440ad057cd17
James Johnson <johnsojr@umn.edu>
2025-10-21 21:44:59 -0500

    fix 419

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,9 @@ use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->validateCsrfTokens(except: [
             '/local-sp/ACS',
+            '/lti/*',
         ]);
 
         // if in development environment, trust proxies since https terminates
