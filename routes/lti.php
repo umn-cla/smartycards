fbda09cec46f5f618d11f108ec492ea1a967e85e
James Johnson <johnsojr@umn.edu>
2025-10-21 22:00:41 -0500

    add simple error page

--- routes/lti.php
+++ routes/lti.php
@@ -7,13 +7,12 @@ use App\Http\Controllers\LtiDeepLinkController;
 use App\Http\Controllers\LtiErrorController;
 use App\Http\Controllers\JwksController;
 
-
 // LTI login, launch, and services
 Route::post('/lti/login', [LtiLoginController::class, 'login'])->name('lti.login');
 Route::post('/lti/launch', [LtiLaunchController::class, 'launch'])->name('lti.launch');
 Route::get('/lti/deep-link', [LtiDeepLinkController::class, 'index'])->name('lti.deep_link');
 Route::post('/lti/deep-link/response', [LtiDeepLinkController::class, 'response'])->name('lti.deep_link.response');
-// Route::get('/lti/error', [LtiErrorController::class, 'index'])->name('lti.error');
+Route::get('/lti/error', [LtiErrorController::class, 'index'])->name('lti.error');
 
 // JWKS endpoint
 Route::get('/.well-known/jwks.json', [JwksController::class, 'keys'])->name('lti.keys');
