893133d6cb70d256aabdd3608a5b5efd5357b68f
James Johnson <johnsojr@umn.edu>
2026-01-06 12:36:49 -0600

    add resource for lti resource link

---
+++ app/Http/Resources/LtiResourceLinkResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LtiResourceLinkResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $membership = $this->ltiResourceLinkMemberships->first();
+
+        return [
+            'id' => $this->id,
+            'resource_link_id' => $this->resource_link_id,
+            'title' => $this->title,
+            'context_id' => $this->context_id,
+            'context_title' => $this->context_title,
+            'context_label' => $this->context_label,
+            'current_user_role' => $membership?->is_staff
+                ? 'staff'
+                : 'student',
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
