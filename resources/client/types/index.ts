db98d577fcfbce3ff6e582aa407997a3ded52cf6
James Johnson <johnsojr@umn.edu>
2026-01-08 16:29:23 -0600

    okey doke

--- resources/client/types/index.ts
+++ resources/client/types/index.ts
@@ -329,7 +329,7 @@ export interface DeckScoreInfo {
   score: number;
   score_maximum: number;
   score_percentage: number;
-  completed_at: ISODateTime;
+  completed_at: ISODateTime | null;
   submitted_at: ISODateTime | null;
   submission_success: boolean | null;
   submission_error: string | null;
@@ -354,6 +354,6 @@ export interface DeckScoreEntry {
 }
 
 export interface DeckScoresResponse {
-  user_role: 'student' | 'staff';
+  user_role: "student" | "staff";
   entries: DeckScoreEntry[];
 }
