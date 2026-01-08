8ac51bc49b76731492ff5ca35dfa7591dadb4357
James Johnson <johnsojr@umn.edu>
2026-01-08 17:37:37 -0600

    update eslint

---
+++ eslint.config.ts
@@ -0,0 +1,56 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+import css from "@eslint/css";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default [
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: ["**/*.css", "public/vendor/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    ...js.configs.recommended,
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parserOptions: { parser: tseslint.parser },
+    },
+  },
+  {
+    files: ["tests/cypress/**/*.{js,ts}"],
+    languageOptions: {
+      globals: {
+        cy: "readonly",
+        Cypress: "readonly",
+        assert: "readonly",
+      },
+    },
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    rules: css.configs.recommended.rules,
+  },
+  eslintConfigPrettier,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "vue/multi-word-component-names": "off",
+      "no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
+];
